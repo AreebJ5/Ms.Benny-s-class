55e33a10bd367fafdcb0cf5df76ef23e321271d9
AreebJ5 <areeb.jackson@patelhs.org>
2020-11-13 02:53:41 +0000

    I added int number of seats 15 elements.

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
- 
+  int[] numSeats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+
+
+
+
+
+
+
   }
 }
 
