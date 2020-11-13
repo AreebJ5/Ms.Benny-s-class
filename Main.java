6fba458cdd1bb87e545af1a3b880a7fe9020f79b
AreebJ5 <areeb.jackson@patelhs.org>
2020-11-13 03:10:01 +0000

    Added a print statement telling the student to sit in their seats

--- Main.java
+++ Main.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
   int[] numSeats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
- Sting[] studentsNames = new String[15];
+ Sting[] studentNames = new String[15];
  //This is where my scanner is 
  Scanner scan = new Scanner(System.in);
- for (int i = 0; i < studentsNames.length; i++)
+ for (int i = 0; i < studentNames.length; i++)
  System.out.println("Enter your name below.");
-
-
+ studentNames [i] = scan.next();
+ int i = 0;
+ while (numSeats [i] <= studentNames.length) 
+ System.out.println( studentNames [i] + " please sit in seat " + numSeats [i]);
+ 
 
   }
 }
