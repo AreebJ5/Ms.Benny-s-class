094af79ebf14ef3db8096a23bf51734068ea7623
AreebJ5 <areeb.jackson@patelhs.org>
2020-11-13 02:50:08 +0000

    Added import scanner

--- Main.java
+++ Main.java
@@ -1,14 +1,7 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
- int[ ] seatNumbers = {-20, -15, 2, 8, 16, 33};
- {
-
-   for (int i = seatNumbers.length / 2; i < seatNumbers.length; i++)
-   {
-      seatNumbers[i] = seatNumbers[i] * 2;
-   }
- }
-  String[] names = new String[15];
+ 
   }
 }
 
