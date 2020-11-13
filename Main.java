2d81678f0ad0627019c289dffca89d5581eb291b
AreebJ5 <areeb.jackson@patelhs.org>
2020-11-13 03:21:00 +0000

    Added extra comments to clean up my code

--- Main.java
+++ Main.java
@@ -5,14 +5,18 @@ class Main {
  String[] studentNames = new String[15];
  //This is where my scanner is 
  Scanner scan = new Scanner(System.in);
+ //This is where my for loop is 
  for (int i = 0; i < studentNames.length; i++)
  {
  System.out.println("Enter your name below.");
+ //Program so students can add their names
  studentNames [i] = scan.next();
  }
  int i = 0;
+ //Added a while loop
  while (numSeats [i] <= studentNames.length) 
    {
+ //Prints out the Students names with their seat number    
  System.out.println( studentNames [i] + " please sit in seat " + numSeats [i]);
  i++;
    }
